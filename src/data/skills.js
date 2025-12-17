abc5772a700c0ef9551d709991ff8b4ae9b3f2ae
ghulammf <muhtadinamri123@gmail.com>
2025-12-17 14:51:21 +0700

    create skills data

---
+++ src/data/skills.js
@@ -0,0 +1,56 @@
+const skills = [
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/500px-HTML5_logo_and_wordmark.svg.png",
+        title: "HTML",
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        title: "CSS",
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        title: "Javascript",
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png",
+        title: "Typescript",
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png",
+        title: "NodeJs",
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/960px-React-icon.svg.png",
+        title: "ReactJs",
+    },
+    {
+        icon: "https://images.seeklogo.com/logo-png/32/2/next-js-logo-png_seeklogo-321806.png",
+        title: "NextJs",
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png",
+        title: "Tailwind CSS",
+    },
+    {
+        icon: "https://hexmos.com/freedevtools/svg_icons/zustand/zustand-original.svg",
+        title: "Zustand",
+    },
+    {
+        icon: "https://cdn.worldvectorlogo.com/logos/expressjs.svg",
+        title: "ExpressJs",
+    },
+    {
+        icon: "https://img.icons8.com/fluent/1200/prisma-orm.jpg",
+        title: "Prisma ORM",
+    },
+    {
+        icon: "https://pngimg.com/d/mysql_PNG9.png",
+        title: "MySQL",
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        title: "Github",
+    },
+];
+
+export default skills
